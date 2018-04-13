cc943ab1f806a8ae3303a1dbe8da9cd57840a521
Павел Кузнецов <pavel@Pavels-Mac-mini.local>
2018-04-13 12:29:40 +0300

    Initial commit;

---
+++ index.js
@@ -0,0 +1,19 @@
+const botToken = process.env.BOT_TOKEN;
+const TelegraBot = require("node-telegram-bot-api");
+
+const options = {
+    polling: {
+        autoStart: false
+    }
+};
+
+const bot = new TelegraBot(botToken, options);
+
+bot.onText(/./, (msg) => console.log(msg.text));
+
+function autoReply(msg)
+{
+    bot.sendMessage(mes.from.id, "I am alive");
+}
+
+bot.startPolling();
