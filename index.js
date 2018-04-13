293d59ca8ebfc65226c0abd7875db4dd3679cb8d
Павел Кузнецов <pavel@Pavels-Mac-mini.local>
2018-04-13 13:50:27 +0300

    try to fix

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ process.env["NTBA_FIX_319"] = 1;
 
 const botToken = process.env.BOT_TOKEN;
 const url = process.env.APP_URL;
+const port = process.env.PORT || 8443;
 const TelegraBot = require("node-telegram-bot-api");
+
 
 const options = {
     polling: {
         webHook: {
-            port: process.env.PORT
+            port: port,
+            host: url
         }
     }
 };
@@ -20,4 +23,5 @@ async function autoReply(msg) {
     await bot.sendMessage(msg.from.id, "I am alive");
 }
 
-bot.setWebHook(`${url}/bot${botToken}`);
+bot.setWebHook();
+//bot.setWebHook(`${url}/bot${botToken}`);
